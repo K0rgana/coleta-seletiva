447e3ba500addd1b262ef1f43211af58663811bd
K0rgana <anagrom1999@gmail.com>
2021-05-24 17:15:01 -0300

    GH-5 add nav component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Nav/>
 </template>
+<script>
+import Nav from '@/components/Nav.vue'
 
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
 <style lang="scss">
+*{
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+ background: #f8f8f8;
+ height: 100vh;
 }
 </style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div id="nav">
+      <h2>Crawler</h2>
+      <div>
+        <router-link to="/">Entrar</router-link>
+        <router-link to="/about">Cadastrar</router-link>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+
+<style lang="scss">
+#nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background: #00ff91;
+  padding: 2vh;
+
+  a {
+    padding: 10px 18px;
+
+    color: #2c3e50;
+    font-weight: bold;
+    text-decoration: none;
+
+    border: #2c3e50 solid 1px;
+    border-radius: 10px;
+    margin-right: 20px;
+
+    &.router-link-exact-active {
+      background: #2c3e50;
+      color: #00ff91;
+    }
+  }
+}
+</style>
