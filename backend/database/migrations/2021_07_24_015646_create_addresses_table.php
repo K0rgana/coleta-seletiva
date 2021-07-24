778bac86df5a2d1681ef6774f4c4e30861cfd0ff
K0rgana <anagrom1999@gmail.com>
2021-07-23 23:36:28 -0300

    GH-18 adiciona migration de endereco

---
+++ backend/database/migrations/2021_07_24_015646_create_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('postalcode', 8)->nullable();
+            $table->string('city');
+            $table->string('district');
+            $table->string('street');
+            $table->string('number');
+            $table->string('reference')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
