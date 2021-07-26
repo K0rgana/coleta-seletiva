25519b4321f702d6a7778bdf5c319d678d0f938f
K0rgana <anagrom1999@gmail.com>
2021-07-26 19:40:58 -0300

    GH-23 atualiza model de usuario

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
     ];
 
@@ -40,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get all of the Ads for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function ads(){
+        return $this->hasMany(Ad::class);
+    }
+    
 }
