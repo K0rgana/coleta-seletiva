25b444fc61369220863e4ef1c89bfa47661db217
K0rgana <anagrom1999@gmail.com>
2021-07-28 22:02:35 -0300

    GH-24 adiciona fabricas no seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use \App\Models\Address;
+use \App\Models\Category;
+use \App\Models\Ad;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+        Category::factory(5)-> create();
+        Address::factory(5)-> create();
+        Ad::factory(15)-> create();
     }
 }
