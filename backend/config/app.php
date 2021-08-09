6d8b25ab582b231d470942e8b8f36692e01bc758
K0rgana <anagrom1999@gmail.com>
2021-08-09 14:41:12 -0300

    GH-25 adiciona biblioteca jwt e configuracoes

--- backend/config/app.php
+++ backend/config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        //jwt auth
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
