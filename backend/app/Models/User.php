4dcbfc6190478f80a1290bfb6e8668f26278d586
K0rgana <anagrom1999@gmail.com>
2021-08-09 14:48:49 -0300

    GH-26 altera model para implementar jwt

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -51,4 +53,23 @@ class User extends Authenticatable
         return $this->hasMany(Ad::class);
     }
     
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
